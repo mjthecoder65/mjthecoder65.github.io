5c9525debb66cf259f9d857ffc10b8b2c8f61b12
Michael Jordan <mjthecoder53161@gmail.com>
2020-12-23 19:41:34 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-profile.addEventListener('mouseover', ()=>{
-    const profile = document.getElementById('profileImage');    
-    profile.src='profile0.jpg';
-});
-profile.addEventListener('mouseout', ()=>{
-    const profile = document.getElementById('profileImage');    
-    profile.src='profile1.jpg'; 
-});
