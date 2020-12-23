44e397263e2fee61625240b0b6346db6d0c37091
Michael Jordan <mjthecoder53161@gmail.com>
2020-12-23 10:34:56 +0900

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
- const button = document.getElementById('submit');
- button.addEventListener('click', submit)
- function submit(event){
-     const name = document.getElementById('name');
-     window.console.log(name.value);
- }
